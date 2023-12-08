c519c8a007b6a50b9fe5aa970fd7df06d4bcde0a
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-08 16:38:45 +0600

    registration controller added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,21 @@
+// import requirements
 import {Routes, Route} from "react-router-dom"
 import './App.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// React toastity
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+// import pages
 import RegistrationPage from "./pages/registrationPage/RegistrationPage"
 import LoginPage from "./pages/loginPage/LoginPage"
+import ActivateUser from "./components/ActivateUserComponent/ActivateUser"
 
+
+
+// app function
 function App() {
 
   return (
@@ -15,6 +23,7 @@ function App() {
       <Routes>
         <Route exact path='/registration' element={<RegistrationPage />}></Route>
         <Route exact path='/login' element={<LoginPage />}></Route>
+        <Route exact path='/activate' element={<ActivateUser />}></Route>
       </Routes>
 
       <ToastContainer
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,9 +1,11 @@
+// import requirements
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// render all app function
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
